d8700335130de917cc7995305d61519aa39c238d
David Friedman <dhfriedman@gmail.com>
2016-07-10 16:31:25 -0700

    initial commit

---
+++ src/juliaOrb.jl
@@ -0,0 +1,148 @@
+module juliaOrb
+
+export true2meanAnom, mean2trueAnom, cart2orb, orb2cart, twoBodyProp
+
+function true2eccAnom(trueAnom, ecc)
+    eccAnom = atan2(sqrt(1-ecc^2)*sin(trueAnom), ecc + cos(trueAnom))
+    return eccAnom
+end
+
+function true2meanAnom(trueAnom, ecc)
+    eccAnom = true2eccAnom(trueAnom, ecc)
+    meanAnom = eccAnom - ecc*sin(eccAnom)
+    return meanAnom
+end
+
+function meanAnomResidual(trueAnom, meanAnom, ecc)
+    residual = meanAnom - true2meanAnom(trueAnom, ecc)
+    return residual
+end
+
+function meanAnomResidualWrtTrue(trueAnom, ecc)
+    # pardon the copy-pasta from Maxima
+    grad = (ecc*sqrt(1-ecc^2)*cos(trueAnom))/sqrt((1-ecc^2)*sin(trueAnom)^2+(cos(trueAnom)+ecc)^2)-(sqrt(1-ecc^2)*sin(trueAnom)^2)/((1-ecc^2)*sin(trueAnom)^2+(cos(trueAnom)+ecc)^2)-
+(sqrt(1-ecc^2)*cos(trueAnom)*(cos(trueAnom)+ecc))/((1-ecc^2)*sin(trueAnom)^2+(cos(trueAnom)+ecc)^2)-
+(ecc*sqrt(1-ecc^2)*sin(trueAnom)*(2*(1-ecc^2)*cos(trueAnom)*sin(trueAnom)-2*(cos(trueAnom)+ecc)*sin(trueAnom)))/(2*((1-ecc^2)*sin(trueAnom)^2+(cos(trueAnom)+ecc)^2)^(3/2));
+    return grad
+end
+
+function mean2trueAnom(meanAnom, ecc)
+    trueAnom = meanAnom
+    tol = 1e-7
+    maxIter = 30
+
+    for i = 1:maxIter
+        residual = meanAnomResidual(trueAnom, meanAnom, ecc)
+        
+        if abs(residual) < tol
+            return trueAnom
+        end
+
+        grad = meanAnomResidualWrtTrue(trueAnom, ecc)
+        trueAnom = trueAnom - residual/grad
+    end
+
+    assert(false)
+
+end
+
+function twoBodyPropOrb(trueAnom, SMA, ecc, mu, delT)
+   meanAnom = true2meanAnom(trueAnom, ecc) 
+   n = sqrt(mu/SMA^3)
+   meanAnom = meanAnom + n*delT
+   trueAnom = mean2trueAnom(meanAnom, ecc)
+   return trueAnom
+end
+
+function twoBodyProp(pos, vel, mu, delT)
+   ecc, inc, RAAN, AOP, trueAnom, SMA = cart2orb(pos, vel, mu)
+   trueAnom = twoBodyPropOrb(trueAnom, SMA, ecc, mu, delT)
+   pos, vel = orb2cart(ecc, inc, RAAN, AOP, trueAnom, SMA, mu)
+   return pos, vel
+end
+
+function cart2orb(pos, vel, mu)
+    r = pos
+    v = vel
+    h = cross(r,v)
+    n = cross([0, 0, 1], h)
+    nMag = norm(n)
+    vMag = norm(v)
+    rMag = norm(r)
+    hMag = norm(h)
+    e = 1/mu*( (vMag^2 - mu/rMag)*r - dot(r,v)*v)
+    eMag = norm(e)
+    zeta = (vMag^2)/2 - mu/rMag
+
+    a = -mu/(2*zeta)
+    p = a*(1 - eMag^2)
+    i = acos(h[3]/hMag)
+
+    if n[1] != 0
+        O = acos(n[1]/nMag)
+    else
+        O = 0
+    end
+
+    if dot(n,e) != 0
+        o = acos(dot(n,e)/(nMag*eMag))
+    else
+        o = 0
+    end
+
+    if dot(e,r) != 0
+        nu = acos(dot(e,r)/(eMag*rMag))
+    else
+        nu = 0
+    end
+
+    ecc = eMag
+    inc = i
+    RAAN = O
+    AOP = o
+    trueAnom = nu
+    SMA = p/(1-eMag^2)
+
+    return ecc, inc, RAAN, AOP, trueAnom, SMA
+end
+
+function orb2cart(ecc, inc, RAAN, AOP, trueAnom, SMA, mu)
+    e = ecc
+    i = inc
+    O = RAAN
+    o = AOP
+    nu = trueAnom
+    p = SMA*(1 - e^2)
+
+    rPQW = [p*cos(nu)/(1 + e*cos(nu)), p*sin(nu)/(1 + e*cos(nu)), 0]
+    vPQW = [-sqrt(mu/p)*sin(nu), sqrt(mu/p)*(e + cos(nu)), 0]
+
+    PQW2IJK = zeros(3,3)
+    cO = cos(O)
+    sO = sin(O)
+    co = cos(o)
+    so = sin(o)
+    ci = cos(i)
+    si = sin(i)
+
+    PQW2IJK[1,1] = cO*co-sO*so*ci
+	PQW2IJK[1,2] = -cO*so-sO*co*ci
+	PQW2IJK[1,3] = sO*si
+	PQW2IJK[2,1] = sO*co+cO*so*ci
+	PQW2IJK[2,2] = -sO*so+cO*co*ci
+	PQW2IJK[2,3] = -cO*si
+	PQW2IJK[3,1] = so*si
+	PQW2IJK[3,2] = co*si
+	PQW2IJK[3,3] = ci
+
+    r = PQW2IJK*rPQW
+    v = PQW2IJK*vPQW
+
+    pos = r
+    vel = v
+
+    return pos, vel
+
+end
+
+end
---
+++ test/testJuliaOrb.jl
@@ -0,0 +1,33 @@
+include("../src/juliaOrb.jl")
+
+using juliaOrb
+
+ecc = 0.02
+trueAnom = 1.5
+@printf("starting ecc: %0.2f\n", ecc)
+@printf("starting true anom: %0.2f rad\n", trueAnom)
+meanAnom = true2meanAnom(trueAnom, ecc)
+@printf("converting to mean anom: %f0.2 rad\n", meanAnom)
+trueAnom = mean2trueAnom(meanAnom, ecc)
+@printf("converting back to true anom: %0.2f rad\n\n", trueAnom)
+
+pos = [-4845, -4014, 2609]*1e3
+vel = [1.26, -5.93, -4.83]*1e3
+mu = 3.986004418e14
+@printf("starting pos: [%0.2f %0.2f %0.2f] m\n", pos[1], pos[2], pos[3])
+@printf("starting vel: [%0.2f %0.2f %0.2f] m/s\n", vel[1], vel[2], vel[3])
+ecc, inc, RAAN, AOP, trueAnom, SMA = cart2orb(pos, vel, mu)
+@printf("ecc: %0.2f inc: %0.2f rad RAAN: %0.2f rad\n", ecc, inc, RAAN)
+@printf("AOP: %0.2f rad trueAnom: %0.2f rad SMA: %0.2f m\n", AOP, trueAnom, SMA)
+pos, vel = orb2cart(ecc, inc, RAAN, AOP, trueAnom, SMA, mu)
+@printf("converting back to pos: [%0.2f %0.2f %0.2f] m\n", pos[1], pos[2], pos[3])
+@printf("converting back to vel: [%0.2f %0.2f %0.2f] m/s\n", vel[1], vel[2], vel[3])
+delT = 1000
+pos, vel = twoBodyProp(pos, vel, mu, delT)
+@printf("pos after 1000 second 2-body propagation: [%0.2f %0.2f %0.2f] m\n", pos[1], pos[2], pos[3])
+@printf("vel after 1000 second 2-body propagation: [%0.2f %0.2f %0.2f] m/s\n", vel[1], vel[2], vel[3])
+pos, vel = twoBodyProp(pos, vel, mu, -delT)
+@printf("pos after -1000 second 2-body propagation: [%0.2f %0.2f %0.2f] m\n", pos[1], pos[2], pos[3])
+@printf("vel after -1000 second 2-body propagation: [%0.2f %0.2f %0.2f] m/s\n\n", vel[1], vel[2], vel[3])
+
+
